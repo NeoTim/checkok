1b02226babfb51c9589bc9e53f42fb7d79c59d95
forsigner <forsigner@gmail.com>
2019-01-03 10:02:43 +0800

    chore: improve bundle task

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,51 @@
 import typescript from 'rollup-plugin-typescript2'
+import fs from 'fs'
+import path from 'path'
 
 import pkg from './package.json'
 
-export default {
-  input: 'src/index.ts',
-  output: [
+const rulesFiles = fs
+  .readdirSync(path.join(__dirname, 'src/rules'))
+  .map(f => `rules/${f.replace('.ts', '')}`)
+
+const files = ['index', ...rulesFiles]
+console.log('files:', files)
+
+const plugins = [
+  typescript({
+    rollupCommonJSResolveHack: true,
+  }),
+]
+
+const getOutput = file => {
+  if (file === 'index') {
+    return [
+      {
+        file: `dist/${file}.js`,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        file: `dist/${file}.es.js`,
+        format: 'es',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ]
+  }
+  return [
     {
-      file: pkg.main,
+      file: `${file}.js`,
       format: 'cjs',
       exports: 'named',
       sourcemap: true,
     },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true,
-    },
-  ],
-  external: [],
-  plugins: [
-    typescript({
-      rollupCommonJSResolveHack: true,
-    }),
-  ],
+  ]
 }
+
+export default files.map(file => ({
+  input: `src/${file}.ts`,
+  output: getOutput(file),
+  plugins,
+}))
